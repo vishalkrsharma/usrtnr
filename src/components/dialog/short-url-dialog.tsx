54d971bcaca8d96bb37ffe1c4daf6de312b49669
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-06-03 23:03:27 +0530

    chore:  - code refactor

--- src/components/dialog/short-url-dialog.tsx
+++ src/components/dialog/short-url-dialog.tsx
@@ -1,24 +1,22 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Url } from '@/generated/prisma';
-import { useAutoTooltip } from '@/hooks/use-auto-tooltip';
 import { useDialogStore } from '@/hooks/use-dialog-store';
 import { EDialogType } from '@/types/dialog';
 import { Copy, ExternalLink } from 'lucide-react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 
 export default function ShortUrlDialog() {
-  const router = useRouter();
   const { type, isOpen, onClose, dialogData } = useDialogStore();
 
   const data = dialogData as Url;
 
+  const shortUrl = process.env.NEXT_PUBLIC_VERCEL_URL + '/' + data?.shortRoute;
+
   const copyShortUrlToClipboard = () => {
-    navigator.clipboard.writeText(process.env.NEXT_PUBLIC_CLIENT_URL + '/' + data.shortRoute);
+    navigator.clipboard.writeText(shortUrl);
   };
 
   if (!data) return null;
@@ -40,8 +38,8 @@ export default function ShortUrlDialog() {
             {data.originalUrl}
           </Link>
           <div className='bg-muted px-4 py-2 rounded-2xl flex justify-between items-center gap-4'>
-            <span>{process.env.NEXT_PUBLIC_CLIENT_URL + '/' + data.shortRoute}</span>
-            <div className='space-x-2'>
+            <div className='truncate max-w-[300px]'>{shortUrl}</div>
+            <div className='space-x-2 flex justify-center items-center gap-2'>
               <Button
                 size='icon'
                 className='rounded-full'
@@ -50,7 +48,7 @@ export default function ShortUrlDialog() {
                 <Copy />
               </Button>
               <Link
-                href={process.env.NEXT_PUBLIC_CLIENT_URL + '/' + data.shortRoute}
+                href={shortUrl}
                 target='_blank'
                 className="inline-flex items-center justify-center gap-2 transition-all [&_svg:not([class*='size-'])]:size-4 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 size-9 rounded-full"
               >
