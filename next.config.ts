b79c49fd9f22679ddfd42a02007eb1fe537e7b7c
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-06-04 21:58:28 +0530

    chore: remove unnecessary field from next config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  experimental: {
-    authInterrupts: true,
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
