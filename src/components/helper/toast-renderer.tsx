5fda1d77ee64cd85f1b4828a02445f37c15f7c58
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-06-15 16:00:39 +0530

    CHANGES
    chore:
     - remove ref code from toast-renderer

--- src/components/helper/toast-renderer.tsx
+++ src/components/helper/toast-renderer.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'sonner';
 
 type TToastVariant = 'success' | 'error' | 'warning' | 'info' | 'loading';
 
 const ToastRenderer = ({ message, variant }: { message: string; variant: TToastVariant }) => {
-  const hasShownToast = useRef(false);
-
   useEffect(() => {
-    if (message && !hasShownToast.current) {
-      hasShownToast.current = true;
-      toast[variant]?.(message) || toast(message);
-    }
-
-    return () => {
-      hasShownToast.current = false;
-    };
+    console.log('aaa');
+    toast[variant]?.(message) || toast(message);
   }, [message, variant]);
 
   return null;
