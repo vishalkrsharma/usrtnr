528a497d5bde5bc088341854caaed3af2995fd63
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-07-06 23:39:42 +0530

    CHANGES
    ui:
     - animated bg for home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,14 +1,16 @@
 import Header from '@/app/(root)/(home)/_components/header';
 import Utils from '@/app/(root)/(home)/_components/utils';
 import UrlShortenerForm from '@/components/forms/url-shortener-form';
+import { BackgroundBeams } from '@/components/ui/background-beams';
 
 const HomePage = () => {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center gap-16'>
+    <main className='min-h-screen flex flex-col items-center justify-center gap-4'>
       <Utils />
       <Header />
       <UrlShortenerForm />
-      <div className='flex-1' />
+
+      <BackgroundBeams />
     </main>
   );
 };
