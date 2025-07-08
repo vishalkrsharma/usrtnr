5c92f8ddc2e2c0051e04511931c7023e244f72eb
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-07-09 01:16:57 +0530

    CHANGES
    fix:
     - make [shortRoute] as public route/ all 11 chars routes are public

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|[a-zA-Z0-9]{7}$).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|[a-zA-Z0-9]{11}$).*)'],
 };
